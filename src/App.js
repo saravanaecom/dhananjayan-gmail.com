24a0963b075c0dd3127eefccce67f1014ff42edf
saravanaecom <saravanaecomweb@gmail.com>
2025-04-28 12:48:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './index.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import AppRouter from './routes/Routes';
+
+
+function App() {
+  return (
+    <>
+
+      <AppRouter/>
+   
+    
+    </>
+  );
+}
+
+export default App;
