d632d3f07694b76d8bd0145b7666444387987025
saravanaecom <saravanaecomweb@gmail.com>
2025-04-28 12:57:07 +0530

    Delete src/logo directory

--- src/logo/AppLogo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Logo from './logo.png';
-const AppLogo = ({ children }) => {
-  return (
-    <img src={Logo} alt="logo" style={{width: '160px', height: '90px'}}/>
-  );
-};
-
-export default AppLogo;
